2384e3b95bac5b1e1dcc58549ab69069402441f5
Admin <bdongo91@gmail.com>
2024-02-13 17:58:12 +0200

    added jquery validation on the form, added logic for splitting full name into first and last name

--- root/resources/js/index.js
+++ root/resources/js/index.js
@@ -1,19 +1,72 @@
-//get REST Countries API data & display in the select field.
+/**IMPLEMENT REST COUNTRIES API CALL AND DISPLAY COUNTRY NAMES IN THE SELECT DROPDOWN FIELD.**/
 const countriesList = document.getElementById("country");
 const getPost = async () => {
-  const response = await fetch("https://restcountries.com/v3.1/independent?status=true");
-  const data = await response.json();
-  return data;
+    const response = await fetch("https://restcountries.com/v3.1/independent?status=true");
+    const data = await response.json();
+    return data;
 };
-
 const displayOption = async () => {
-  const options = await getPost();
-  for (option of options) {
-    const newOption = document.createElement("option");
-    newOption.value = option.name.common;
-    newOption.text = option.name.common;
-    countriesList.appendChild(newOption);
-  }
+    const options = await getPost();
+    for (option of options) {
+        const newOption = document.createElement("option");
+        newOption.value = option.name.common;
+        newOption.text = option.name.common;
+        countriesList.appendChild(newOption);
+    }
+};
+displayOption();
+
+/** SPLIT FULL NAME INTO FIRST NAME & LAST NAME AND POST ALL FORM DATA TO A LOCAL CSV FILE**/
+let saveFile = () => {
+
+    // Get the data from each element on the form.
+    const fullname = document.getElementById("fullname").value;
+    const email = document.getElementById("email");
+    const country = document.getElementById("country");
+    const cellphone = document.getElementById("cellphone");
+
+    let tmpArray = fullname.split(' '); 
+    const lastname = tmpArray.pop(); 
+    const firstname = tmpArray.join(' '); 
+
+    // The data variable stores all the data to be posted to a CSV file.
+    let data = "\r First Name: " + firstname + " \r " + "Last Name: " + lastname + " \r " +"Email: " + email.value + " \r" +"Country: " + country.value + " \r" + "Cellphone: " + cellphone.value + " \r ";
+    const textToBLOB = new Blob([data], { type: "text/csv" }); // Convert the text to BLOB.
+    var filename = new Date();
+    var month = new Date(); //months from 1-12
+    month = month.getMonth();
+
+    var day = new Date();
+    var day = day.getUTCDate();
+
+    var year = new Date();
+    var year = year.getUTCFullYear();
+
+    newdate = year + "/" + month + "/" + day;
+    const sFileName = filename; // The file to save the data to.
+
+    let newLink = document.createElement("a");
+    newLink.download = new Date();
+
+    if (window.webkitURL != null) {
+        newLink.href = window.webkitURL.createObjectURL(textToBLOB);
+    } else {
+        newLink.href = window.URL.createObjectURL(textToBLOB);
+        newLink.style.display = "none";
+        document.body.appendChild(newLink);
+    }
+
+    newLink.click(); //autodownload the CSV file and save it on the machine.
+    
 };
 
-displayOption();
+
+
+
+
+
+
+
+
+
+
---
+++ root/resources/js/validation.js
@@ -0,0 +1,39 @@
+jQuery(document).ready(function(){
+    
+    $(function() {
+    // Setup form validation on the #register-form element
+   $("form[name='registration-form']").validate({  
+
+       // Specify the validation rules
+  rules: {
+           fullname: "required", //fullname field is mandatory 
+           email: {               
+            required: true, //email is required
+            email: true     //email is in valid format
+        }, 
+        country: {
+          required: true, //country field is mandatory 
+          
+        },
+        cellphone: {    
+            required: true, //cellphone number is mandatory
+            maxlength:10    //cellphone number must be 10 digits long
+        },  
+       },
+       // Specify the validation error messages
+  messages: {
+           fullname: "Please Enter Full Name",
+           email: "Please Enter Valid Email Address",
+           country: "Please Select Your Country",
+           cellphone:"Please Enter a Valid Cellphone Number"
+       },
+       submitHandler: function(form) {
+           form.submit();
+           saveFile();
+           window.location("success.html");
+       }
+   });
+  
+    });
+  })
+   
